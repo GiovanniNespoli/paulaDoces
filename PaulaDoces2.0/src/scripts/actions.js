cf85a2cfe31f69362393f83680b327ce982607d6
Giovanni Nespoli <ggnespoli@gmail.com>
2022-11-22 23:52:08 -0300

    refact: change request

--- PaulaDoces2.0/src/scripts/actions.js
+++ PaulaDoces2.0/src/scripts/actions.js
@@ -1,11 +1,55 @@
 document.addEventListener('click', (event) => {
     if (!event.target.matches('#cakeButton')) return;
 
-    console.log('TRESTE');
-
     const element = document.getElementById("cakesClosed");
 
     element.classList.toggle("cakesOpened")
 
     console.log(element);
 }, false);
+
+document.addEventListener('click', (event) => {
+    if (!event.target.matches('#cadyButton')) return;
+
+    const element = document.getElementById("candyClosed");
+
+    element.classList.toggle("candyOpened")
+
+    console.log(element);
+}, false);
+
+document.addEventListener('click', (event) => {
+    if (!event.target.matches('#salgadosButton')) return;
+
+    const element = document.getElementById("salgadosClosed");
+
+    element.classList.toggle("salgadosOpened")
+
+    console.log(element);
+}, false);
+
+async function selected(valor, name) {
+
+    var verify = localStorage.getItem(valor);
+
+    if (verify) {
+        console.log("entrou1");
+        document.getElementsByName(name).forEach(element => {
+            element.style["border"] = "none";
+        });
+        localStorage.removeItem(valor);
+
+        return valor
+    }
+
+    if (!verify) {
+        console.log("entrou2");
+        document.getElementsByName(name).forEach(element => {
+            element.style["border"] = "lightgreen 0.5rem solid";
+            element.style["borderRadius"] = "1rem";
+        });
+        localStorage.setItem(valor, valor);
+        return valor
+    }
+}
+
